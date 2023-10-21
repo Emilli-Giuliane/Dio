bcba301c63fb84d7190cfe7aa768f96623bc6a3e
user.email <emilligiuliane@gmail.com>
2023-10-21 16:45:06 -0300

    Implements e interface

---
+++ Implements-IPhone/Iphon/src/Fucionalidades.java
@@ -0,0 +1,17 @@
+public interface Funcionalidades {
+    // Métodos de reprodução de música
+    String tocarMusica();
+    String pausarMusica();
+    String selecionarMusica();
+
+    // Métodos de chamadas telefônicas
+    void fazerLigacao();
+    void atenderChamada();
+    void iniciarCorreioVoz();
+
+    // Métodos de navegação na internet
+    void abrirNavegador();
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
---
+++ Implements-IPhone/Iphon/src/IPhone.java
@@ -0,0 +1,45 @@
+
+public class IPhone implements Funcionalidades {
+    @Override
+    public String tocarMusica() {
+        return "Tocando música...";
+    }
+
+    @Override
+    public String pausarMusica() {
+        return "Música pausada.";
+    }
+
+    @Override
+    public String selecionarMusica() {
+        return "Música selecionada.";
+    }
+
+    @Override
+    public void fazerLigacao() {
+    }
+
+    @Override
+    public void atenderChamada() {
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+    }
+
+    @Override
+    public void abrirNavegador() {
+    }
+
+    @Override
+    public void exibirPagina() {
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+    }
+
+    @Override
+    public void atualizarPagina() {
+    }
+}
